b578932ea901bcff13118989340e836446ad7a46
sonjoypaul079 <paul.kumar.sonjoy@gmail.com>
2020-02-16 23:49:27 +0600

    sqrt upto 3 decimal point

--- LeetCode/69-Sqrt(x).cpp
+++ LeetCode/69-Sqrt(x).cpp
@@ -1,28 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int binarySqrt(int x, int low, int high)
+double binarySqrt(int x, double low, double high)
 {
 	if (high <= low)
 		return low;
 
-	long long int mid = (low + high) / 2;
+	double mid = (low + high) / 2;
 
-	if (mid * mid <= x && (mid + 1) * (mid + 1) > x)
+	if (mid * mid <= x && (mid + .001) * (mid + .001) > x)
 	{
 		return mid;
 	}
 	else if (mid * mid > x)
 	{
-		return binarySqrt(x, low, mid - 1);
+		return binarySqrt(x, low, mid - .001);
 	}
 	else
 	{
-		return binarySqrt(x, mid + 1, high);
+		return binarySqrt(x, mid + .001, high);
 	}
 }
 
-int mySqrt(int x)
+double mySqrt(int x)
 {
 	return binarySqrt(x, 0, x);
 }
@@ -31,7 +31,7 @@ int main()
 {
 	int x = 10;
 
-	printf("Square Root=%d\n", mySqrt(x));
+	printf("Square Root=%f\n", mySqrt(x));
 
 	return 0;
 }
